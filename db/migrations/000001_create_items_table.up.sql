aeb5c6c8e3b7c7f6f2684a1ab22b1db5f08722a6
Bari Arviv <bararviv0120@gmail.com>
2022-07-23 16:24:29 +0300

    Fixed 000001_create_items_table.up.sql
    Signed-off-by: Bari Arviv <bararviv0120@gmail.com>

--- db/migrations/000001_create_items_table.up.sql
+++ db/migrations/000001_create_items_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS users(
     email            VARCHAR(200) PRIMARY KEY,
     username         VARCHAR(50) NOT NULL,
     password         VARCHAR(50) NOT NULL,
-    sys_created_date TIMESTAMP with time zone NOT NULL DEFAULT now(),
+    sys_created_date TIMESTAMP with time zone NOT NULL DEFAULT now()
 );
